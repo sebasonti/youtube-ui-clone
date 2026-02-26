420844bdf18eeec8527eb1f1b7fd6f8d90a0b5f1
Juan Sebastián Ontiveros Gonzales <sebas.onti94@gmail.com>
2026-02-25 21:56:05 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  );
+}
+
+export default App;
