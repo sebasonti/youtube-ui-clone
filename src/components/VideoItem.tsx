5bab5933d85c9fbc47e4df3256cbd036d1ac5907
Juan Sebastián Ontiveros Gonzales <sebas.onti94@gmail.com>
2026-03-06 10:50:14 -0600

    Video preview

--- src/components/VideoItem.tsx
+++ src/components/VideoItem.tsx
@@ -1,6 +1,7 @@
 import type { Video } from '@/types/Video';
 import { formatDuration } from '@/utils/formatDuration';
 import { formatTimeAgo } from '@/utils/formatTimeAgo';
+import { useEffect, useRef, useState } from 'react';
 
 const VIEWS_FORMATTER = Intl.NumberFormat(undefined, { notation: 'compact' });
 
@@ -16,16 +17,41 @@ const VideoItem = ({
   videoUrl,
   views,
 }: Props) => {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current == null) return;
+
+    if (isVideoPlaying) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, [isVideoPlaying]);
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2"
+      onMouseEnter={() => setIsVideoPlaying(true)}
+      onMouseLeave={() => setIsVideoPlaying(false)}
+    >
       <a href={`/watch?v=${id}`} className="relative aspect-video">
         <img
           src={thumbnailUrl}
-          className="block w-full h-full object-cover rounded-xl"
+          className={`block w-full h-full object-cover transition[border-radius] duration-200 ${isVideoPlaying ? 'rounded-none' : 'rounded-xl'}`}
         />
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
           {formatDuration(duration)}
         </div>
+        <video
+          className={`block h-full object-cover absolute inset-0 transition-opacity duration-200 ${isVideoPlaying ? 'opacity-100 delay-200' : 'opacity-0'}`}
+          src={videoUrl}
+          ref={videoRef}
+          muted
+          playsInline
+        ></video>
       </a>
       <div className="flex gap-2">
         <a href={`/@${channel.id}`} className="shrink-0">
